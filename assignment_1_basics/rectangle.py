db28d86735281907087e0a402e80d4beec8ac70c
Elizabeth Korzhova <elizabeth.korzhova.coop@gmail.com>
2025-12-24 04:37:36 +0200

    feat: implement Rectangle class and test all methods

---
+++ assignment_1_basics/rectangle.py
@@ -0,0 +1,35 @@
+"""This module provides a simple class to represent a rectangle."""
+
+class Rectangle:
+    """Represent the Rectangle with a width and a height."""
+    def __init__(self, width: int|float, height: int|float) -> None:
+        """Initialize the Rectangle with the width and height."""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self) -> int|float:
+        """Get the width of the rectangle."""
+        return self.__width
+
+    @property
+    def height(self) -> int|float:
+        """Get the height of the rectangle."""
+        return self.__height
+
+    def area(self) -> int|float:
+        """Calculate the area of the rectangle."""
+        return self.__width * self.__height
+
+    def perimeter(self) -> int|float:
+        """Calculate the perimeter of the rectangle."""
+        return 2 * (self.__width + self.__height)
+
+    def is_square(self) -> bool:
+        """Check if the rectangle is square."""
+        return self.__width == self.__height
+
+    def resize(self, new_width: int|float, new_height: int|float) -> None:
+        """Resize the rectangle to a new width and height."""
+        self.__width = new_width
+        self.__height = new_height
---
+++ assignment_1_basics/test.py
@@ -0,0 +1,22 @@
+"""
+This module is designed for testing the functionality of the Rectangle class and
+correctness of work calculate_circle_area function
+"""
+from rectangle import Rectangle
+from math_functions import calculate_circle_area
+
+# test subtask 1
+print("The area of the circle is: ",
+      calculate_circle_area(float(input("Enter the radius of the circle: "))))
+
+# test subtask 2
+rectangle_abcd = Rectangle(10.4, 7.9)
+print(f"\nThe rectangle with the width of {rectangle_abcd.width} and the height "
+      f"of {rectangle_abcd.height}")
+print("The area of the rectangle is: ", rectangle_abcd.area())
+print("The perimeter of the rectangle is: ", rectangle_abcd.perimeter())
+print("Is the rectangle the square? ", rectangle_abcd.is_square())
+rectangle_abcd.resize(25, 25)
+print(f"The width and the height of the rectangle were changed to {rectangle_abcd.width} "
+      f"and {rectangle_abcd.height} respectively")
+print("Is the rectangle a square?", rectangle_abcd.is_square())
