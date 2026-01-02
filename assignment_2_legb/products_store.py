abe07e1ca4a5810816b8934ffe85749b9507441e
Elizabeth Korzhova <elizabeth.korzhova.coop@gmail.com>
2026-01-02 16:19:32 +0200

    refactor: change return type for change_price func

--- assignment_2_legb/products_store.py
+++ assignment_2_legb/products_store.py
@@ -6,7 +6,7 @@ from typing import Callable
 
 
 def create_product(name: str, price: float, quantity: int) -> tuple[
-    Callable[[], dict[str, str | float | int]], Callable[[float], dict[str, str | float | int]]]:
+    Callable[[], dict[str, str | float | int]], Callable[[float], None]]:
     """Function to create a product with given name, price and quantity."""
     product: dict[str, str | int | float] = {
         "name": name,
@@ -14,11 +14,10 @@ def create_product(name: str, price: float, quantity: int) -> tuple[
         "quantity": quantity
     }
 
-    def change_price(new_price: float) -> dict[str, str | float | int]:
+    def change_price(new_price: float) -> None:
         """Function to change the price of the product."""
         product["price"] = new_price
         print(f"Price changed to {new_price}")
-        return product
 
     def get_product() -> dict[str, str | float | int]:
         """Function to get the product information."""
