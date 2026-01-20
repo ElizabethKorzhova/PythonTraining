be8ce1730bb77ef954e2cc617fc49eab64cfad6a
Elizabeth Korzhova <elizabeth.korzhova.coop@gmail.com>
2026-01-20 19:11:47 +0200

    refactor: extend type annotation for get_mean_from_file func

--- assignment_4_oop_exception/handling_file.py
+++ assignment_4_oop_exception/handling_file.py
@@ -2,15 +2,16 @@
 and calculating their mean."""
 
 import os
+from typing import Tuple, List, Union
 
 
-def get_mean_from_file(file_path: str) -> tuple:
+def get_mean_from_file(file_path: str) -> Tuple[Union[List[float], None], Union[str, float]]:
     """Gets list of numbers and their arithmetic mean from the given text file."""
     try:
         if os.path.exists(file_path) and os.path.getsize(file_path) == 0:
             raise ValueError("Error: File is empty")
         with open(file_path, "r", encoding="utf-8") as file:
-            numbers = []
+            numbers: List[float] = []
             for line in file:
                 parts = line.split()
                 for part in parts:
