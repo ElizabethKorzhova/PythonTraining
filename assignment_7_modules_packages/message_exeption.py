0f235ac54ff3788bc17492e7a23877e8a1457709
Elizabeth Korzhova <elizabeth.korzhova.coop@gmail.com>
2026-03-09 12:49:17 +0100

    feat: implement message system that send message via all services and handle exeption

---
+++ assignment_7_modules_packages/message_exeption.py
@@ -0,0 +1,12 @@
+"""Module provides custom exceptions MessageSendingException that raised
+when messages won't be sent."""
+
+
+class MessageSendingException(Exception):
+    """Exception raised when messages won't be sent."""
+
+    def __init__(self, service_name: str, details: str) -> None:
+        """Initializes MessageSendingException class with service_name and details to
+        insert into the exception."""
+        self.service_name, self.details = service_name, details
+        super().__init__(f"Error when sending message through {service_name}: {details}")
