64f608aae3dd1c0a26184103713c9e1a5a3eeb7f
Elizabeth Korzhova <elizabeth.korzhova.coop@gmail.com>
2026-05-02 18:29:43 +0300

    docs: add docstrings to products and utils modules

--- assignment_11_nosql/utils.py
+++ assignment_11_nosql/utils.py
@@ -1,6 +1,17 @@
-from pymongo import MongoClient
+"""This module provides helper functions for managing database"""
+from typing import Tuple
 
-def get_db_collections():
+from pymongo import MongoClient
+from pymongo.database import Database
+from pymongo.collection import Collection
+
+def get_db_collections() -> Tuple[Database, Collection, Collection]:
+    """
+    Create a MongoDB client and return database, products and orders collection
+
+    Returns:
+        Tuple[Database, Collection, Collection]: MongoDB database and MongoDB collections
+    """
     client = MongoClient("localhost", 27017)
     db = client.shop
     return db, db.products, db.orders
